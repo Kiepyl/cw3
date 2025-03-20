dcf9a4d3ef43daa2a237d481cd557c76e951cd77
Kamil Trzykowski <s33609@pjwstk.edu.pl>
2025-03-20 12:46:41 +0100

    update

--- Zadanie1.java
+++ Zadanie1.java
@@ -1,5 +1,23 @@
-public class Zadanie1 {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Zadanie1 {
+    public String fName;
+    public String lName;
+    public String indexNum;
+    public String email;
+    public String address;
+    public List<Double> grades;
+
+    public Zadanie1(String fName, String lName, String indexNum, String email, String address) {
+        this.fName = fName;
+        this.lName = lName;
+        this.indexNum = indexNum;
+        this.email = email;
+        this.address = address;
+        this.grades = new ArrayList<>();
+    }
+    public void addGrade(double grade) {
+        grades.add(grade);
     }
 }
---
+++ Zadanie1_2.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zadanie1_2 {
+    public String nazwa;
+    public List<Zadanie1> students;
+    public Zadanie1_2(String nazwa) {
+        this.nazwa = nazwa;
+        this.students = new ArrayList<>();
+    }
+}
